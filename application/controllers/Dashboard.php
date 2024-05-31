44c9544bba2be91e02c72977b3968f9d59b44e7b
carollousdev <carollous.webdev@gmail.com>
2024-05-31 17:02:49 +0700

    Hapus default file CodeIgniter 3 dan setup core template

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+require APPPATH . '/core/CI_Template.php';
+class Dashboard extends CI_Template
+{
+    public function index()
+    {
+        parent::index();
+    }
+}
