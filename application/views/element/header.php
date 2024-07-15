41e5e0ec6995d6b25a43572b344d5830c95a1d5a
carollousdev <carollous.webdev@gmail.com>
2024-07-15 22:28:33 +0700

    Change position logout button

--- application/views/element/header.php
+++ application/views/element/header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $dashboard->title ?></title>
 
-    <link rel="icon" type="image/x-icon" href="http://localhost:8080/template/dist/img/AdminLTELogo.png">
+    <link rel="icon" type="image/x-icon" href="http://localhost:8080/template/dist/img/pelindo.png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="http://localhost:8080/template/plugins/datatables/bootstrap5/css/bootstrap.min.css">
@@ -36,19 +36,23 @@
             </ul>
             <!-- Right navbar links -->
             <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() . 'logout' ?>">
+                        <i class="fas fa-solid fa-lock"></i> Logout</a>
+                </li>
             </ul>
         </nav>
 
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <a href="dashboard" class="brand-link">
-                <img src="http://localhost:8080/template/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <img src="http://localhost:8080/template/dist/img/pelindo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">SAP Surveyor</span>
             </a>
 
             <div class="sidebar">
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="http://localhost:8080/template/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="http://localhost:8080/template/dist/img/user.svg" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block"><?= $_SESSION['name'] ?></a>
@@ -72,14 +76,6 @@
                     </ul>
                 </nav>
                 <nav class="mt-2" style="position: absolute;bottom: 0;">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
-                        <li class="nav-item">
-                            <a href="<?= base_url() . 'logout' ?>" class="nav-link">
-                                <i class="nav-icon fas fa-reguler fa-arrow-right"></i>
-                                <p>Logout</p>
-                            </a>
-                        </li>
-                    </ul>
                 </nav>
             </div>
         </aside>
