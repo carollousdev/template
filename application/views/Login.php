d576e2260f4031082b89027adb8f464b2055696c
Carollous Dachi <166882001+carollousdev@users.noreply.github.com>
2024-09-07 04:10:12 +0700

    Create favicon in login page

--- application/views/Login.php
+++ application/views/Login.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminLTE 3 | Log in</title>
 
+    <link rel="icon" type="image/x-icon" href="http://localhost:8080/template/dist/img/pelindo.png">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
